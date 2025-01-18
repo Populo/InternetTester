56042fc1e724e8959ddd7154dd20320928409a92
dave hotdog <25019718+Populo@users.noreply.github.com>
2025-01-18 13:10:01 -0500

    refactor - check google, modem, and router

--- InternetTest/Tester.cs
+++ InternetTest/Tester.cs
@@ -1,45 +1,74 @@
 ﻿using System.Net;
 using System.Net.NetworkInformation;
 
-Console.WriteLine("Starting...");
+Console.WriteLine($"[{DateTime.Now}] Starting...");
 Console.ForegroundColor = ConsoleColor.Red;
-var p = new Ping();
-bool failed = false;
 
 byte i = 0;
 while (true)
 {
-    // google
-    var ping = p.Send(IPAddress.Parse("142.250.217.174"));
-
-    if (ping.Status != IPStatus.Success)
+    var outside = new Destination
     {
-        Console.WriteLine($"Ping failed at {DateTime.Now}");
-        failed = true;
-    }
-    else if (ping.RoundtripTime > 200)
+        Name = "Google",
+        Address = IPAddress.Parse("142.250.217.174"),
+        WarningTime = 400
+    };
+    var modem = new Destination
     {
-        Console.WriteLine($"Roundtrip time too long. Took {ping.RoundtripTime}ms at: {DateTime.Now}");
-        failed = true;
-    }
-    else
+        Name = "Modem",
+        Address = IPAddress.Parse("10.0.0.1"),
+        WarningTime = 100
+    };
+    var router = new Destination
     {
-        if (failed)
-        {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Back to normal.");
-            Console.ForegroundColor = ConsoleColor.Red;
-            failed = false;
-        }
-    }
+        Name = "Router",
+        Address = IPAddress.Parse("192.168.68.1"),
+        WarningTime = 100
+    };
+    
+    Test(outside);
+    Test(modem);
+    Test(router);
 
     ++i;
     if (i == byte.MaxValue)
     {
         Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("Still running...");
+        Console.WriteLine($"[{DateTime.Now}] Still running...");
         Console.ForegroundColor = ConsoleColor.Red;
         i = 0;
     }
     Thread.Sleep(1000);
+}
+
+void Test(Destination dest)
+{
+    var ping = new Ping().Send(dest.Address);
+
+    if (ping.Status != IPStatus.Success)
+    {
+        Console.WriteLine($"[{DateTime.Now}] {dest.Name} - Ping failed");
+        dest.Failed = true;
+    }
+    else if (ping.RoundtripTime > dest.WarningTime)
+    {
+        Console.WriteLine($"[{DateTime.Now}] {dest.Name} - Slow ping {ping.RoundtripTime}ms");
+        dest.Failed = true;
+    }
+    else
+    {
+        if (!dest.Failed) return;
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Back to normal.");
+        Console.ForegroundColor = ConsoleColor.Red;
+        dest.Failed = false;
+    }
+}
+
+internal class Destination
+{
+    public required string Name { get; init; }
+    public required IPAddress Address { get; init; }
+    public required int WarningTime { get; init; }
+    public bool Failed { get; set; } = false;
 }
